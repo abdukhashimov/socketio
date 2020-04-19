b97d16f3decdec522354ce4c06cc5b1a20a2b7c7
abdukhashimov <abdukhashimov@yandex.ru>
2020-04-19 18:51:20 +0500

    did not understand it

--- asyncio_lear_docs.py
+++ asyncio_lear_docs.py
@@ -1,12 +1,20 @@
 import asyncio
 
 
-async def nested():
-    return 42
+async def factorial(name, number):
+    f = 1
+    for i in range(2, number + 1):
+        print(f"Task {name}: Compute factorial({i})...")
+        asyncio.sleep(1)
+        f *= i
+    print(f"Task {name}: factorial({number}) = {f}")
 
 
 async def main():
-
-    print(await nested())
+    await asyncio.gather(
+        factorial("C", 4),
+        factorial("B", 3),
+        factorial("A", 2),
+    )
 
 asyncio.run(main())
