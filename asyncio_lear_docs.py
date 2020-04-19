ebe5f1595db86e83881dbac9727bc0656f6526e8
abdukhashimov <abdukhashimov@yandex.ru>
2020-04-19 18:37:23 +0500

    learned how to create asyncio tasks

--- asyncio_lear_docs.py
+++ asyncio_lear_docs.py
@@ -8,11 +8,19 @@ async def say_after(delay, what):
 
 
 async def main():
+    task1 = asyncio.create_task(
+        say_after(1, "Hello")
+    )
+
+    task2 = asyncio.create_task(
+        say_after(2, "World")
+    )
+
     print(f"started at {time.strftime('%X')}")
 
-    await say_after(2, "Hello")
-    await say_after(1, "Wolrd")
+    # Wait until both tasks are completed (should take
+    # around 2 seconds.)
+    await task1
+    await task2
 
     print(f"finished at {time.strftime('%X')}")
-
-asyncio.run(main())
